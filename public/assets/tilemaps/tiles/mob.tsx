c78e55d5ae2498b6b3fbf4fe2817aa433da2ddf5
Walton Family <waltonfamily@Macintoshs-Mac-mini.local>
2018-02-27 10:49:16 -0500

    more

---
+++ public/assets/tilemaps/tiles/mob.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="mob" tilewidth="20" tileheight="20" tilecount="8" columns="8">
+ <image source="../../sprites/mob.png" width="160" height="20"/>
+</tileset>
