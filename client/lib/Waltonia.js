9204a020b1ec87013d9d6259ab9e0fdc331a218b
Walton Family <waltonfamily@Macintoshs-Mac-mini.local>
2018-03-09 21:32:30 -0500

    direction animations

--- client/lib/Waltonia.js
+++ client/lib/Waltonia.js
@@ -9,11 +9,11 @@ class Waltonia extends Phaser.Game {
       type: Phaser.CANVAS,
       parent: 'content',
       // Need to make the following divisible by 20 to match the tiles or else we get some weird choppiness
-      width: 2 * Math.round(Math.floor(window.innerWidth * window.devicePixelRatio/2) / 2),
-      height: 2 * Math.round(Math.floor(window.innerHeight * window.devicePixelRatio/2) / 2)-4,
+      width: 2 * Math.round(Math.floor(window.innerWidth * window.devicePixelRatio/2) / 3),
+      height: 2 * Math.round(Math.floor(window.innerHeight * window.devicePixelRatio/2) / 3)-4,
       backgroundColor: '#2d2d2d',
       pixelArt: true,
-      zoom: 2,
+      zoom: 3,
       physics: {
           default: 'arcade',
           arcade: {
--- client/lib/objects/Character.js
+++ client/lib/objects/Character.js
@@ -12,6 +12,7 @@ class Character extends Phaser.GameObjects.Sprite {
     this.following = null
     this.followedBy = null
     this.movementDuration = 150
+    this.texture = texture
 
     scene.physics.world.enable(this)
     this.setOrigin(0,0)
@@ -21,6 +22,30 @@ class Character extends Phaser.GameObjects.Sprite {
 
     this.highlight = new CharacterHighlight({scene, x: tile.pixelX, y: tile.pixelY})
     this.highlightEnabled = true
+    this.scene.anims.create({
+      key: 'left',
+      frames: this.scene.anims.generateFrameNumbers(this.texture, { start: 6, end: 7 }),
+      frameRate: 10,
+      repeat: -1
+    });
+    this.scene.anims.create({
+      key: 'right',
+      frames: this.scene.anims.generateFrameNumbers(this.texture, { start: 2, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    });
+    this.scene.anims.create({
+      key: 'up',
+      frames: this.scene.anims.generateFrameNumbers(this.texture, { start: 0, end: 1 }),
+      frameRate: 10,
+      repeat: -1
+    });
+    this.scene.anims.create({
+      key: 'down',
+      frames: this.scene.anims.generateFrameNumbers(this.texture, { start: 4, end: 5 }),
+      frameRate: 10,
+      repeat: -1
+    });
 
   }
 
@@ -156,6 +181,7 @@ class Character extends Phaser.GameObjects.Sprite {
 
   moveToByPath(callback) {
     if (!this.movementPath || this.movementPath.length == 0) {
+      this.anims.stop();
       this.moving = false
       if (callback) {
         callback();
@@ -177,6 +203,17 @@ class Character extends Phaser.GameObjects.Sprite {
         this.movementPath.shift()
         this.tile = toTile
         this.moveToByPath.call(this, callback)
+      },
+      onStart: () => {
+        if (toTile.x < fromTile.x) {
+          this.anims.play('left', true);
+        } else if (toTile.x > fromTile.x) {
+          this.anims.play('right', true);
+        } else if (toTile.y < fromTile.y) {
+          this.anims.play('up', true);
+        } else if (toTile.y > fromTile.y) {
+          this.anims.play('down', true);
+        }
       }
     });
   }
