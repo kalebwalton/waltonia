2b56c94a8feffa4dc776bd0784da194502a974b6
Walton Family <waltonfamily@Macintoshs-Mac-mini.local>
2018-04-13 21:52:30 -0400

    got external tilesets loadinggit add .

--- client/lib/MapScene.js
+++ client/lib/MapScene.js
@@ -31,7 +31,54 @@ class MapScene extends Phaser.Scene {
     console.log("===MAIN SCENE PRELOAD===")
     this.movement = new Movement(this)
     this.url = new URL(window.location.href);
-    this.load.tilemapTiledJSON(this.getKey(), `assets/maps/defs/_${this.mapConfig.name}_${this.mapConfig.type}_${this.mapConfig.level}.json`);
+
+
+    var customTilemapJSONFile = (key, url, path, format, xhrSettings) => {
+      var json = new Phaser.Loader.FileTypes.JSONFile(key, url, path, xhrSettings);
+      json.type = 'tilemapJSON';
+      json.tilemapFormat = format;
+      var onProcess = function(callback) {
+        this.state = Phaser.Loader.FILE_PROCESSING;
+        this.data = JSON.parse(this.xhrLoader.responseText);
+
+        // Need to pass the URL through so we can create a relative path to any external
+        // source files like tilesets.
+        this.data.url = url
+
+        for (let i=0;i<this.data.tilesets.length;i++) {
+          let tileset = this.data.tilesets[i]
+
+          // Remove tileset.source so tilemap parser doesn't bomb out
+          tileset.sourceBackup = tileset.source
+          delete tileset.source
+
+          // // Add another file to the loader before all files are loaded
+          // loader.addFile(customTilesetJSONFile(key+"_tileset_"+i, tilemapURI.pathname, path, format, xhrSettings))
+
+        }
+        this.onComplete();
+        callback(this);
+      }
+      json.onProcess = onProcess.bind(json)
+
+      return json;
+    }
+
+    Phaser.Loader.FileTypesManager.register('customTilemapJSONFile', function (key, url, xhrSettings) {
+        if (Array.isArray(key)) {
+            for (var i = 0; i < key.length; i++) {
+                //  If it's an array it has to be an array of Objects, so we get everything out of the 'key' object
+                this.addFile(customTilemapJSONFile(key[i], url, this.path, Phaser.Tilemaps.Formats.TILED_JSON, xhrSettings));
+            }
+        } else {
+            this.addFile(customTilemapJSONFile(key, url, this.path, Phaser.Tilemaps.Formats.TILED_JSON, xhrSettings));
+        }
+
+        return this;
+    });
+    Phaser.Loader.FileTypesManager.install(this.load)
+
+    this.load.customTilemapJSONFile(this.getKey(), `assets/maps/defs/${this.mapConfig.name}_${this.mapConfig.type}_${this.mapConfig.level}.json`);
     this.load.image(this.mapConfig.type, `assets/maps/tiles/combined/${this.mapConfig.name}_${this.mapConfig.type}.png`);
     this.load.spritesheet('player', 'assets/sprites/players/female_warrior.png', { frameWidth: 20, frameHeight: 20 });
     this.load.spritesheet('mob', 'assets/sprites/mobs/little_flare.png', { frameWidth: 20, frameHeight: 20 });
@@ -39,7 +86,90 @@ class MapScene extends Phaser.Scene {
 
   }
 
+  create()
+  {
+    this.load.removeAllListeners()
+    this.load.once('complete', this.myInit, this);
+
+    var customTilesetJSONFile = (tilemapKey, key, url, path, format, xhrSettings) => {
+      var json = new Phaser.Loader.FileTypes.JSONFile(key, url, path, xhrSettings);
+
+      var onProcess = function(callback) {
+        this.state = Phaser.Loader.FILE_PROCESSING;
+        this.data = JSON.parse(this.xhrLoader.responseText);
+        this.data.tilemapKey = tilemapKey
+        this.onComplete();
+        callback(this);
+      }
+      var onComplete = function () {
+          if (this.linkFile) {
+              if (this.linkFile.state === Phaser.Loader.FILE_WAITING_LINKFILE) {
+                  //  The linkfile has finished processing, and is waiting for this file, so let's do them both
+                  this.state = Phaser.Loader.FILE_COMPLETE;
+                  this.linkFile.state = Phaser.Loader.FILE_COMPLETE;
+                  this.loader.emit('filecomplete', this)
+              } else {
+                  //  The linkfile still hasn't finished loading and/or processing yet
+                  this.state = Phaser.Loader.FILE_WAITING_LINKFILE;
+              }
+          } else {
+              this.state = Phaser.Loader.FILE_COMPLETE;
+              this.loader.emit('filecomplete', this)
+          }
+      }
+      json.onProcess = onProcess.bind(json)
+      json.onComplete = onComplete.bind(json)
+
+      return json;
+    }
+
+    Phaser.Loader.FileTypesManager.register('customTilesetJSONFile', function (tilemapKey, key, url, xhrSettings) {
+        if (Array.isArray(key)) {
+            for (var i = 0; i < key.length; i++) {
+                //  If it's an array it has to be an array of Objects, so we get everything out of the 'key' object
+                this.addFile(customTilesetJSONFile(tilemapKey, key[i], url, this.path, Phaser.Tilemaps.Formats.TILED_JSON, xhrSettings, this));
+            }
+        } else {
+            this.addFile(customTilesetJSONFile(tilemapKey, key, url, this.path, Phaser.Tilemaps.Formats.TILED_JSON, xhrSettings, this));
+        }
+        return this;
+    });
+    Phaser.Loader.FileTypesManager.install(this.load)
+
+    var tm = this.cache.tilemap.get(this.getKey()).data
+    var tss = tm.tilesets
+    for (let i=0;i<tss.length;i++) {
+      let ts = tss[i]
+      let loc = new URL(window.location.href)
+      let tilemapURI = new URL(tm.url.substr(0,tm.url.lastIndexOf("/")+1)+ts.sourceBackup, loc)
+      this.load.customTilesetJSONFile(this.getKey(), this.getKey()+"_ts_"+i, tilemapURI.pathname)
+    }
+
+    this.load.on('filecomplete', (file) => {
+      // Just check the data for a key of 'tilemapKey' which indicates it's a tileset
+      if (file.data.tilemapKey) {
+        var ts = file.data
+        ts.firstgid = 1
+        // Merge into the associated tilemap
+        var tm = this.cache.tilemap.get(ts.tilemapKey).data
+        tm.tilesets[0] = ts
+        console.log(tm)
+      }
+    })
+
+    this.load.start()
+  }
+
+  myInit() {
+    this.initMap();
+    this.initEasystar();
+    this.initPlayer();
+    this.initInput();
+    this.events.emit("create")
+  }
+
   initMap() {
+
     this.map = this.make.tilemap({ key: this.getKey(), tileWidth: 20, tileHeight: 20 });
     console.log(this.map)
     var tileset = this.map.addTilesetImage(this.mapConfig.type);
@@ -132,21 +262,6 @@ class MapScene extends Phaser.Scene {
     }
   }
 
-  create()
-  {
-
-    console.log("===CREATE MAIN SCENE===");
-
-    this.initMap();
-    this.initEasystar();
-    this.initPlayer();
-    this.initInput();
-    this.events.emit("create")
-
-    // Set up the player to collide with the tilemap layer. Alternatively, you can manually run
-    // collisions in update via: this.physics.world.collide(player, layer).
-
-  }
 
   update(time, delta) {
 
